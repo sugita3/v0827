c762db4470d5f0409025b3751c34a6d113f2d69a
satoshigp <satoshi3104>
2020-08-27 09:43:33 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0827
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+
+        
+        }
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+            Point mp = MousePosition;
+            mp = PointToClient(mp);
+            label2.Text = "" + mp.X + "," + mp.Y;
+            
+        }
+    }
+}
